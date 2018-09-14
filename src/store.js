8ee39deaedddd6b28c80f2d3b602b9b77a766124
xXZang3tsuXx <pasindudil1995@gmail.com>
2018-09-14 11:34:08 +0530

    added saga & refactored

--- src/store.js
+++ src/store.js
@@ -2,17 +2,24 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk';
 import rootReducers from './reducers';
 
-const middleware = [thunk]
+import createSagaMiddleware from 'redux-saga';
+
+//create saga middleware
+const sagaMiddleware = createSagaMiddleware();
+
+const middleware = [thunk, sagaMiddleware]
 
 const initialState = {}
+
+// for debugging using the browser extension
+const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 const store = createStore(
     rootReducers, 
     initialState, 
     compose(
         applyMiddleware(...middleware),
-        // for debugging using the browser extension
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        reduxDevTools
     ));
     
 export default store
